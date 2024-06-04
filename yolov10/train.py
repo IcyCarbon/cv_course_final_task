4f81914462650481ae87f87b0f96f6c5d14d26c2
IcyCarbon <Exhibkoro@gmail.com>
2024-06-04 20:00:23 +0800

    Add v10s Adam

--- yolov10/train.py
+++ yolov10/train.py
@@ -21,8 +21,8 @@ if __name__ == '__main__':
                 # iou=0.6,
                 lr0=0.001,
                 batch=-1,
-                #shear=0.2,
-                #mixup=0.2,
+                shear=0.2,
+                mixup=0.2,
                 # close_mosaic=10,  # 是一个特定于YOLOv8的参数，用于控制何时停止使用mosaic数据增强（一种图像数据增强方法）
                 # workers=0,  # 用于加载数据的工作线程数。设置为0表示数据加载将在主线程中进行。
                 device='0',  # 指定训练使用的设备，'0'通常表示使用第一个GPU
